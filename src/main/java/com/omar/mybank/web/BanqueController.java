3f1854d92f31b5615cf3c0862009efcf7e652a04
farhanio <omar.farhani@outlook.com>
2018-02-22 16:37:59 +0100

    First Controller

---
+++ src/main/java/com/omar/mybank/web/BanqueController.java
@@ -0,0 +1,21 @@
+package com.omar.mybank.web;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.omar.mybank.metier.IBanqueMetier;
+
+@Controller
+public class BanqueController {
+	@Autowired
+	private IBanqueMetier banqueMetier;
+	
+	@RequestMapping("/operations")
+	public String index(){
+		return "comptes";
+	}
+	
+	
+
+}
