af3c48671194627a8a1d6b97ab9ca04ba041339c
farhanio <omar.farhani@outlook.com>
2018-02-28 17:38:27 +0100

    .gitignore file update

--- src/main/java/com/omar/mybank/web/BanqueController.java
+++ src/main/java/com/omar/mybank/web/BanqueController.java
@@ -3,7 +3,7 @@ package com.omar.mybank.web;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMapping; 
 import java.util.List;
 
 import com.omar.mybank.entities.Compte;
